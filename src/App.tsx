d64e8deffff4181f324e804dd4d6763c5cc4e390
Sungkuk Kim <sungkuk5420@gmail.com>
2023-11-21 23:42:28 +0900

    보호된 라우터 처리.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import reset from "styled-reset"
 import { useEffect, useState } from "react"
 import LoadingScreen from "./components/loading-screen"
 import { auth } from "./firebase"
+import ProtectedRoute from "./components/protected-route"
 
 const router = createBrowserRouter ([
   {
@@ -17,11 +18,11 @@ const router = createBrowserRouter ([
     children : [
       {
         path : "",
-        element: <Home/>,
+        element: <ProtectedRoute><Home/></ProtectedRoute>,
       },
       {
         path : "profile",
-        element: <Profile/>,
+        element: <ProtectedRoute><Profile/></ProtectedRoute>,
       }
     ]
   },
